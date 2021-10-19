b9fe5fa0ec7fb00683f15ce26f61b2c0c7708d34
CitizenOne <17427385+CitizenOneX@users.noreply.github.com>
2021-10-19 22:49:47 +1100

    added some logging to print out active device and stream info. Added check for valid pipeline before attempting to process frames

--- Filters/RealSenseCam.cpp
+++ Filters/RealSenseCam.cpp
@@ -2,18 +2,56 @@
 
 HRESULT RealSenseCam::Init()
 {
+	// TODO work out how to get this logging into Debug Output console in VS2019
+	rs2::log_to_console(RS2_LOG_SEVERITY_DEBUG);
+	rs2::log(RS2_LOG_SEVERITY_DEBUG, "Starting Init()");
+
 	// TODO might use data from the profile at initialisation, e.g. frame dimensions etc.
 	m_pColorizer = new rs2::colorizer();
 	m_pPipeline = new rs2::pipeline();
 
 	m_pCfg = new rs2::config();
-	//m_pCfg->enable_stream(RS2_STREAM_DEPTH);
-	m_pCfg->enable_stream(RS2_STREAM_INFRARED);
-	m_pProfile = &m_pPipeline->start();//*m_pCfg);
+	m_pCfg->disable_all_streams();
+	m_pCfg->enable_stream(RS2_STREAM_DEPTH, 320, 240, RS2_FORMAT_Z16, 30);
+	m_pCfg->enable_stream(RS2_STREAM_INFRARED, 320, 240, RS2_FORMAT_Y8, 30);
+	
+	if (m_pCfg->can_resolve(((std::shared_ptr<rs2_pipeline>)*m_pPipeline)))
+	{
+		m_pProfile = &m_pPipeline->start(*m_pCfg);  
+		// TODO I think this profile isn't the same as what is returned by get_active_profile()...
+		// Could be that I'm not handling the std::shared_ptr<rs2_pipeline_profile>() smart pointer business properly
+		
+		// Debug logging to work out which streams we got in our profile
+		OutputDebugStringA("Pipeline Profile: \n");
+		rs2::pipeline_profile activeProfile = m_pPipeline->get_active_profile();
 
-	//m_pAlignToDepth = new rs2::align(RS2_STREAM_DEPTH);
+		OutputDebugStringA("- Device: ");
+		OutputDebugStringA(activeProfile.get_device().get_info(rs2_camera_info::RS2_CAMERA_INFO_NAME));
+		OutputDebugStringA("\n");
 
-	return *m_pProfile ? S_OK : E_FAIL;
+		if (*m_pProfile)
+		{
+			auto streamProfiles = activeProfile.get_streams();
+
+			for each (auto streamProfile in streamProfiles)
+			{
+				OutputDebugStringA("- Stream: ");
+				OutputDebugStringA(rs2_stream_to_string(streamProfile.stream_type()));
+				OutputDebugStringA(", ");
+				char buffer[4];
+				OutputDebugStringA(_itoa(streamProfile.fps(), buffer, 10));
+				OutputDebugStringA(" fps, ");
+				OutputDebugStringA(rs2_format_to_string(streamProfile.format()));
+				OutputDebugStringA("\n");
+			}
+
+			//m_pAlignToDepth = new rs2::align(RS2_STREAM_DEPTH);
+
+			return S_OK;
+		}
+	}
+
+	return E_FAIL;
 }
 
 void RealSenseCam::UnInit()
@@ -26,27 +64,29 @@ void RealSenseCam::UnInit()
 
 void RealSenseCam::GetCamFrame(BYTE* frameBuffer, int frameSize)
 {
-	// Block program until frames arrive
-	rs2::frameset frames = m_pPipeline->wait_for_frames();
+	if (m_pPipeline != NULL)
+	{
+		// Block program until frames arrive
+		rs2::frameset frames = m_pPipeline->wait_for_frames();
 
-	// align the color frame to the depth frame (so we end up with the smaller depth frame with color mapped onto it)
-	//frames = m_pAlignToDepth->process(frames);
-	//auto depth = frames.get_depth_frame();
-	// colorize the depth data with the default color map
-	//auto colorized_depth = m_pColorizer->colorize(depth);
-	auto ir = frames.get_infrared_frame();
+		// align the color frame to the depth frame (so we end up with the smaller depth frame with color mapped onto it)
+		//frames = m_pAlignToDepth->process(frames);
+		//auto depth = frames.get_depth_frame();
+		// colorize the depth data with the default color map
+		//auto colorized_depth = m_pColorizer->colorize(depth);
+		auto ir = frames.get_infrared_frame();
 
-	// TODO For now just copy the colorized depth frame over to the framebuffer
-	// wait, this could have been a single memcpy...
-	//for (int i = 0; i < min(frameSize, colorized_depth.get_data_size()); i++)
-	//{
-	//	frameBuffer[i] = ((BYTE*)colorized_depth.get_data())[i];
-	//}
-	//memcpy(frameBuffer, colorized_depth.get_data(), min(frameSize, colorized_depth.get_data_size()));
-	memcpy(frameBuffer, ir.get_data(), min(frameSize, ir.get_data_size()));
+		// TODO For now just copy the colorized depth frame over to the framebuffer
+		// wait, this could have been a single memcpy...
+		//for (int i = 0; i < min(frameSize, colorized_depth.get_data_size()); i++)
+		//{
+		//	frameBuffer[i] = ((BYTE*)colorized_depth.get_data())[i];
+		//}
+		//memcpy(frameBuffer, colorized_depth.get_data(), min(frameSize, colorized_depth.get_data_size()));
+		memcpy(frameBuffer, ir.get_data(), min(frameSize, ir.get_data_size()));
 
-	// TODO - plenty:
-	// fetch RGB and Depth frames as point cloud
-	// project colorized point cloud to the 2D frame
-
+		// TODO - plenty:
+		// fetch RGB and Depth frames as point cloud
+		// project colorized point cloud to the 2D frame
+	}
 }
