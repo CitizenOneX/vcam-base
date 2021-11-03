be003569660e0deecddbd96424bfe52d68e9dfd1
CitizenOne <17427385+CitizenOneX@users.noreply.github.com>
2021-11-03 20:09:30 +1100

    switch to PointCloud type for simplicity

--- Filters/Filters.h
+++ Filters/Filters.h
@@ -19,7 +19,7 @@ public:
     IFilterGraph *GetGraph() {return m_pGraph;}
 
     RealSenseCam m_realSenseCam;
-    RealSenseCamType m_type = RealSenseCamType::PointCloudColor;
+    RealSenseCamType m_type = RealSenseCamType::PointCloud;
     bool m_connected;
 
 private:
